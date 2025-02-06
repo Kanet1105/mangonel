9cb96a1028fd0848131aba6c4be614383599f77b
kanet1105 <zombiedelah@gmail.com>
2025-02-06 11:20:05 +0900

    Wrap 'xdp_desc' struct

--- mangonel-libxdp-rs/src/descriptor.rs
+++ mangonel-libxdp-rs/src/descriptor.rs
@@ -2,39 +2,40 @@ use mangonel_libxdp_sys::xdp_desc;
 
 use crate::socket::Socket;
 
+#[derive(Clone, Copy, Debug)]
 pub struct Descriptor {
     address: u64,
     length: u32,
-    socket: Socket,
 }
 
-impl From<(&xdp_desc, &Socket)> for Descriptor {
-    fn from(value: (&xdp_desc, &Socket)) -> Self {
+impl From<&xdp_desc> for Descriptor {
+    fn from(value: &xdp_desc) -> Self {
         unsafe {
             Self {
                 address: (*value.0).addr,
                 length: (*value.0).len,
-                socket: value.1.clone(),
             }
         }
     }
 }
 
 impl Descriptor {
+    #[inline(always)]
     pub fn address(&self) -> u64 {
         self.address
     }
 
+    #[inline(always)]
     pub fn length(&self) -> u32 {
         self.length
     }
 
     #[inline(always)]
-    pub fn get_data(&mut self) -> &mut [u8] {
-        let headroom_size = self.socket.umem().umem_config().frame_headroom;
-        let address = self.address - headroom_size as u64;
+    pub fn get_data(&mut self, socket: &Socket) -> &mut [u8] {
+        let headroom_size = socket.umem().umem_config().frame_headroom;
+        let address = self.address - headroom_size;
         let length = self.length + headroom_size;
-        let offset = self.socket.umem().get_data(address) as *mut u8;
+        let offset = socket.umem().get_data(address) as *mut u8;
 
         unsafe { std::slice::from_raw_parts_mut(offset, length as usize) }
     }
