98ac94227a8adbfd8cc57c7c0a5036a574547f68
kanet <zombiedelah@gmail.com>
2025-06-25 03:23:47 +0900

    Restructured

--- mangonel-libxdp-rs/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod descriptor;
-pub mod mmap;
-pub mod ring_buffer;
-pub mod socket;
-pub mod umem;
-pub mod util;
--- mangonel-libxdp-sys/wrapper.h
+++
@@ -1,3 +0,0 @@
-#include <bpf/bpf.h>
-#include <xdp/xsk.h>
-#include <xdp/libxdp.h>
