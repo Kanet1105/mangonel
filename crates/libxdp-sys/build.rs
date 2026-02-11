a32f6bf43da71b69d45671eeeee5d5c590e4b26c
DH Lee <zombiedelah@gmail.com>
2026-02-12 04:13:50 +0900

    fix the build script

--- crates/libxdp-sys/build.rs
+++ crates/libxdp-sys/build.rs
@@ -1,55 +1,32 @@
 use bindgen::Builder;
-use std::{env, fs, os::unix, path::PathBuf};
+use mangonel_util::system;
+use std::{env, path::PathBuf};
 
-const XDP_TOOLS: &str = "../../xdp-tools";
-const LIBBPF: &str = "lib/libbpf/src";
-const LIBXDP: &str = "lib/libxdp";
 const WRAPPER: &str = "wrapper.h";
 
-fn check_os() {
-    if !cfg!(target_os = "linux") {
-        panic!("Currently supports linux only.");
-    }
-}
-
 fn main() {
-    check_os();
-
-    let package_root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let xdp_tools = package_root.join(XDP_TOOLS).canonicalize().expect(
-        "xdp-tools directory not found - did you run `git submodule update --init --recursive`?",
-    );
-    let libbpf = xdp_tools.join(LIBBPF);
-    let libxdp = xdp_tools.join(LIBXDP);
-    let headers_path = xdp_tools.join("headers");
-
-    // Create a symlink bpf -> libbpf/src in headers/ so that <bpf/libbpf.h> resolves correctly
-    // xsk.h includes <bpf/libbpf.h> which expects headers/bpf/libbpf.h
-    let bpf_symlink = headers_path.join("bpf");
-    if !bpf_symlink.join("libbpf.h").exists() {
-        // Remove existing bpf directory/symlink if it doesn't have libbpf.h
-        let _ = fs::remove_dir_all(&bpf_symlink);
-        let _ = fs::remove_file(&bpf_symlink);
-        unix::fs::symlink(&libbpf, &bpf_symlink)
-            .expect("Failed to create bpf symlink in headers directory");
-    }
+    system::check_system_info().unwrap();
+    let default_lib_paths = system::default_library_paths().unwrap();
 
     // Tell cargo where to find the static libraries
-    println!("cargo::rustc-link-search=native={}", libbpf.display());
+    for lib_path in default_lib_paths {
+        println!("cargo::rustc-link-search=native={}", lib_path.display());
+    }
     println!("cargo::rustc-link-lib=static=bpf");
-    println!("cargo::rustc-link-search=native={}", libxdp.display());
     println!("cargo::rustc-link-lib=static=xdp");
     println!("cargo::rerun-if-changed={WRAPPER}");
 
+    let default_include_paths = system::default_include_paths().unwrap();
+
     // Generate bindings with clang include paths
-    let bindings = Builder::default()
+    let mut builder = Builder::default()
         .header(WRAPPER)
-        // Include path for <xdp/xsk.h>, <xdp/libxdp.h>, and <bpf/libbpf.h> (via symlink)
-        .clang_arg(format!("-I{}", headers_path.display()))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate_inline_functions(true)
-        .generate()
-        .expect("Unable to generate bindings");
+        .generate_inline_functions(true);
+    for include_path in default_include_paths {
+        builder = builder.clang_arg(format!("-I{}", include_path.display()));
+    }
+    let bindings = builder.generate().expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
